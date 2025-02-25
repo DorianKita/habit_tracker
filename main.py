b85f925dedccab8628c8c8f61d4ab0cd567859bd
Dorian Kita <dorian.kita90@gmail.com>
2025-02-25 20:32:27 +0100

    created graph request

--- main.py
+++ main.py
@@ -17,3 +17,15 @@ user_params = {
 #
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_config = {
+    'id': 'graph1',
+    'name': 'Cycling',
+    'unit': 'Km',
+    'type': 'float',
+    'color': 'sora'
+}
+
+graph_endpoint = 'https://pixe.la/v1/users/dolan/graphs'
+
+response = requests.post(url=graph_endpoint, json=graph_config)
